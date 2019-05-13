748c723b2b2157b268e3070ab9750c9308476908
Aimez vous <uengao@126.com>
2019-05-13 10:19:29 +0800

    first commit

---
+++ src/routes.js
@@ -0,0 +1,146 @@
+import Login from './views/Login.vue'
+import NotFound from './views/404.vue'
+import Home from './views/Home.vue'
+import Main from './views/Main.vue'
+import Table from './views/nav1/Table.vue'
+import Form from './views/nav1/Form.vue'
+import user from './views/nav1/user.vue'
+import Page4 from './views/nav2/Page4.vue'
+import Page5 from './views/nav2/Page5.vue'
+import Page6 from './views/nav3/Page6.vue'
+import echarts from './views/charts/echarts.vue'
+import Ho from './views/Ho.vue'
+
+let routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
+    //{ path: '/main', component: Main },
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-home',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/main', component: Main, name: '首页' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '学生管理',
+        iconCls: 'fa fa-user-circle-o',//图标样式class
+        children: [
+            
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '考试管理',
+        iconCls: 'fa fa-search',
+        children: [
+            { path: '/page4', component: Page4, name: '页面4' },
+            { path: '/page5', component: Page5, name: '页面5' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-line-chart',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/page6', component: Page6, name: '课程管理' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '社团管理',
+        iconCls: 'fa fa-suitcase',
+        children: [
+            { path: '/echarts', component: echarts, name: 'echarts' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '人事管理',
+        iconCls: 'fa fa-eye',//图标样式class
+        children: [
+            
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '财务管理',
+        iconCls: 'fa fa-dollar',//图标样式class
+        children: [
+            
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '总务管理',
+        iconCls: 'fa fa-server',//图标样式class
+        children: [
+           
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '后勤管理',
+        iconCls: 'fa fa-support',//图标样式class
+        children: [
+            
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '公告管理',
+        iconCls: 'fa fa-bullhorn',//图标样式class
+        children: [
+            
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '*',
+        hidden: true,
+        redirect: { path: '/404' }
+    }
+];
+
+export default routes;
---
+++ src/views/ho.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        主页
+    </section>
+</template>
